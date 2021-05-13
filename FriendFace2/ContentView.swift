ca9442cc5a926d11fe70ff1715c39a3ef5887938
Brandon Knox <bknox.dev@gmail.com>
2021-05-13 05:18:15 -0700

    Initial Commit

---
+++ FriendFace2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FriendFace2
+//
+//  Created by Brandon Knox on 5/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
