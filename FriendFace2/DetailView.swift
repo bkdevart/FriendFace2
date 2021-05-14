06749aaab6e64dee8fa661a813073e530067f6c5
Brandon Knox <bknox.dev@gmail.com>
2021-05-13 17:55:03 -0700

    Completed layout from original Struct FriendFace DetailView

--- FriendFace2/DetailView.swift
+++ FriendFace2/DetailView.swift
@@ -12,11 +12,9 @@ struct DetailView: View {
     @FetchRequest(entity: User.entity(), sortDescriptors: []) var users: FetchedResults<User>
     
     let user: User
-//    let friends: [User]
-//    let users: [User]  // make moc?
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack {
             Text(user.wrappedName)
                 .font(.largeTitle)
                 .fontWeight(.bold)
@@ -27,56 +25,22 @@ struct DetailView: View {
             Text(user.wrappedAbout)
             Text("Member since \(user.formattedRegisteredDate)")
                 .font(.footnote)
-//            Text("Friends:")
-            
-//                ForEach(user.friendsArray) { friend in
-//                    Text(friend.wrappedName)
-//                }
-//            }
-            
-
             NavigationView {
-                VStack {
-                    ForEach(user.friendsArray) { friend in
-                        // need to look up id off of User and pass to DetailViews
-                        NavigationLink(destination: DetailView(user: friend)) {
-                            Text(friend.wrappedName)
-                                .font(.headline)
-                            Text(friend.wrappedCompany)
-                                .foregroundColor(.secondary)
-                        }
+                List(user.friendsArray) { friend in
+                    NavigationLink(destination: DetailView(user: friend)) {
+                        Text(friend.wrappedName)
+                            .font(.headline)
+                        Text(friend.wrappedCompany)
+                            .foregroundColor(.secondary)
                     }
-                    .navigationTitle("\(user.wrappedName)'s friends")  // pull first name only
                 }
+                .navigationTitle("\(user.wrappedName)'s friends")  // pull first name only
             }
         }
         .padding(.horizontal)
     }
     
-//    func findFriends(friends: [Friend]) -> [User] {
-//        // return a list of User items representing this user's friends
-//        var userFriends = [User]()
-//
-//        for user in friends {
-//            if let match = users.first(where: { $0.id == user.id}) {
-//                userFriends.append(match)
-//            } else {
-//                fatalError("Missing \(user.wrappedName)")
-//            }
-//        }
-//
-//        return userFriends
-//    }
-    
     init(user: User) {
         self.user = user
-//        self.friends = friends
-//        self.users = users
     }
 }
-
-//struct DetailView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        DetailView()
-//    }
-//}
